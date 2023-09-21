9c0c86ce865659550c3e90d3833b9a4ff07328c6
Kiet <vankietclbk@gmail.com>
2023-09-21 20:20:54 +0700

    feat: success response

--- src/v1/controllers/access.controller.js
+++ src/v1/controllers/access.controller.js
@@ -1,11 +1,14 @@
 "use strict";
 
-const  AccessService  = require("../services/access.service");
+const { CREATED } = require("../core/success.response");
+const AccessService = require("../services/access.service");
 
 class AccessController {
   signUp = async (req, res, next) => {
-      console.log("P::signUp::", req.body);
-      return res.status(201).json(await AccessService.signUp(req.body));
+    return new CREATED({
+      message: "Register successfully",
+      metadata: await AccessService.signUp(req.body),
+    }).send(res);
   };
 }
 module.exports = new AccessController();
