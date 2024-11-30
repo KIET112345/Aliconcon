e315ab2535b5c6c14a9370ddb397a1bfad7538ec
KIET112345 <vankietclbk@gmail.com>
2024-11-30 11:33:17 +0700

    feat: add discount service and commitlint

---
+++ src/v1/services/discount.service.js
@@ -0,0 +1,64 @@
+"use strict";
+
+import { covertToObjectIdMongodb } from "../utils";
+
+const { NotFoundError } = require("../core/error.response");
+const discount = require("../models/discount.model");
+
+export class DiscountService {
+  static async createDiscount(payload) {
+    const {
+      code,
+      start_date,
+      end_date,
+      is_active,
+      shopId,
+      min_order_value,
+      product_ids,
+      applies_to,
+      name,
+      description,
+      type,
+      value,
+      max_value,
+      max_uses,
+      max_uses_per_user,
+      uses_count
+    } = payload;
+
+    if (new Date < new Date(start_date) || new Date > new Date(end_date)) {
+        throw NotFoundError('Discount code has expired')
+    }
+
+    const foundDiscount = await discount.findOne({
+        discount_code: code,
+        discount_shopId: covertToObjectIdMongodb(shopId),
+    }).lean();
+
+    if (!foundDiscount || !foundDiscount.is_active) {
+        throw new NotFoundError('Discount not exist');
+    }
+
+    const newDiscount = await discount.create({
+        discount_name: name,
+        discount_description: description,
+        discount_type: type,
+        discount_code: code,
+        discount_value: value,
+        discount_min_order_value: min_order_value,
+        discount_max_value: max_value,
+        discount_start_date: start_date,
+        discount_end_date: end_date,
+        discount_max_uses: max_uses,
+        discount_uses_count: uses_count,
+        discount_users_used: users_used,
+        discount_shopId: shop_id,
+        discount_max_uses_per_user: max_uses_per_user,
+        discount_is_active: is_active,
+        discount_applies_to_user: applies_to_user,
+        discount_product_ids: applies_to === 'all'  ? [] : product_ids,
+    });
+
+    return newDiscount;
+  }
+}
